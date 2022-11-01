e5a439ed57e6cc8ab55c3df5daeaf89390dcb519
Justin Bealer <jmbealer11@gmail.com>
2022-11-01 01:01:19 -0500

    i think frontend is passing

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
+import ReservationForm from "../reservations/ReservationForm";
 import { today } from "../utils/date-time";
 
 /**
@@ -18,8 +19,8 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
+      <Route path="/reservations/new">
+        <ReservationForm />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
