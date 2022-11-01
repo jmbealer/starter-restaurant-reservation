a0d411a57782f63dd4071a811a1585abf3216248
Justin Bealer <jmbealer11@gmail.com>
2022-10-31 21:39:26 -0500

    working us-01

---
+++ back-end/src/errors/isValidNumOfPeople.js
@@ -0,0 +1,12 @@
+function isValidNumOfPeople(req, res, next) {
+  const { data: { people } = {} } = req.body;
+
+  if (typeof people !== "number" || people < 1) {
+    next({
+      status: 400,
+      message: `people should be an integer greater than 0`,
+    });
+  } else next();
+}
+
+module.exports = isValidNumOfPeople;
--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,41 @@
-/**
- * List handler for reservation resources
- */
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperty = require("../errors/hasProperty");
+const isValidDate = require("../errors/isValidDate");
+const isValidTime = require("../errors/isValidTime");
+const isVaildNumOfPeople = require("../errors/isValidNumOfPeople");
+
+// List handler for reservation resources
 async function list(req, res) {
+  const { data } = req.query;
+  const listOfRes = await service.list(date);
+
   res.json({
-    data: [],
+    data: listOfRes,
+  });
+}
+
+async function create(req, res) {
+  const { data } = req.body;
+  const createdReservation = await service.create(data);
+
+  res.status(201).json({
+    data: createdReservation,
   });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
+  create: [
+    hasProperty("first_name"),
+    hasProperty("last_name"),
+    hasProperty("mobile_number"),
+    hasProperty("reservation_date"),
+    isValidDate,
+    hasProperty("reservation_time"),
+    isValidTime,
+    hasProperty("people"),
+    isVaildNumOfPeople,
+    asyncErrorBoundary(create),
+  ],
 };
