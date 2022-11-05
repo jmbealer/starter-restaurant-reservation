b3d03219f5e4a6510267c1a003a9840be6113604
Justin Bealer <jmbealer11@gmail.com>
2022-11-05 02:05:23 -0500

    working on backend

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,7 +1,9 @@
-const tables = require("./01-tables.json");
+const tables = require("./01-tables.json")
 
 exports.seed = function(knex) {
   return knex
     .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(() => knex("tables").insert(tables));
+    .then(function () {
+          return knex("tables").insert(tables);
+        });
 };
